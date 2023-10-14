ad2f06023b60c89bba509fb398ece129d284d4ae
BenHSiegel <b.siegel@yale.edu>
2023-10-14 17:02:56 -0400

    Update center_finder.m

--- center_finder.m
+++ center_finder.m
@@ -1,7 +1,7 @@
-t = Tiff('20230524 ch0 spread 21-29MHz mask.tif');
+t = Tiff('10-13-23 array size calibration pic 2 lower power threshhold.tif');
 image = read(t);
 image = image(:,:,1);
-[cent cm] = FastPeakFind(image,60);
+[cent, cm] = FastPeakFind(image,60);
 mesh(cm);
 
 %% 
@@ -24,35 +24,74 @@ xpos = xpos.';
 ypos = ypos.';
 centers = [xpos ypos];
 
-centerssort = sortrows(centers,1,'descend')
+centerssort = sortrows(centers,1,'descend');
 %% 
 
-freq = [-4;-3;-2;-1;0;1;2;3;4];
-xdisp = (centerssort(:,1) - centerssort(4,1)) .* 5.2;
-ydisp = (centerssort(:,2) - centerssort(4,2)) .* 5.2;
-fit0 = polyfit(freq, xdisp, 1);
-xfit = fit0(1)*freq + fit0(2);
+freq0 = [22;22;22;22;22;22;22;22;22;22;
+    23;23;23;23;23;23;23;23;23;23;
+    24;24;24;24;24;24;24;24;24;24;
+    25;25;25;25;25;25;25;25;25;25;
+    26;26;26;26;26;26;26;26;26;26;
+    27;27;27;27;27;27;27;27;27;27];
+freq1 = [23;23.5;24;24.5;25;25.5;26;26.5;27;27.5;
+    23;23.5;24;24.5;25;25.5;26;26.5;27;27.5;
+    23;23.5;24;24.5;25;25.5;26;26.5;27;27.5;
+    23;23.5;24;24.5;25;25.5;26;26.5;27;27.5;
+    23;23.5;24;24.5;25;25.5;26;26.5;27;27.5;
+    23;23.5;24;24.5;25;25.5;26;26.5;27;27.5];
+xdisp = (xpos(:) - xpos(35)) .* 4.8;
+ydisp = (ypos(:) - ypos(35)) .* 4.8;
+fit0 = polyfit(freq0, xdisp, 1);
+xfit = fit0(1)*freq0 + fit0(2);
 xfitin = fit0(1)/7;
-fit1 = polyfit(freq, ydisp, 1);
-yfit = fit1(1)*freq + fit1(2);
+fit1 = polyfit(freq1, ydisp, 1);
+yfit = fit1(1)*freq1 + fit1(2);
 yfitin = fit1(1)/7;
 f1 = figure;
-plot(freq, xdisp,'*');
+plot(freq0, xdisp,'*');
 hold on;
-plot(freq, xfit, '--');
-title('CH0 Displacement vs frequency On Axis');
+plot(freq0, xfit, '--');
+title('CH0 Displacement vs frequency');
 xlabel('CH0 Output Frequency (MHz)');
 ylabel('Displacement from Center (um)');
 fitlabel = append(num2str((fit0(1))),' um/MHz ->', num2str((xfitin)), ' um/MHz inside');
-legend('Spot center', fitlabel, 'Location', 'southwest');
+legend('Spot center', fitlabel, 'Location', 'southeast');
 
 f2 = figure;
-plot(freq, ydisp, '*');
+plot(freq1, ydisp, '*');
 hold on;
-plot(freq, yfit, '--');
+plot(freq1, yfit, '--');
+title('CH1 Displacement vs Frequency');
+xlabel('CH1 Output Frequency (MHz)');
+ylabel('Displacement from Center (um)');
+fitlabel = append(num2str((fit1(1))),' um/MHz ->', num2str((yfitin)), ' um/MHz inside');
+legend('Spot center', fitlabel, 'Location', 'southeast');
+
+
+fit3 = polyfit(freq1(11:20), xdisp(11:20), 1);
+xfitoff = fit3(1)*freq1(11:20) + fit3(2);
+xfitoffcham = fit3(1)/7;
+fit4 = polyfit(freq0([1,11,21,31,41,51]), ydisp([1,11,21,31,41,51]), 1);
+yfitoff = fit4(1)*freq0([1,11,21,31,41,51]) + fit4(2);
+yfitoffcham = fit4(1)/7;
+
+f3 = figure;
+plot(freq1(11:20), xdisp(11:20),'*');
+hold on;
+plot(freq1(11:20), xfitoff, '--');
+title('CH1 Displacement vs frequency Off Axis');
+xlabel('CH1 Output Frequency (MHz)');
+ylabel('Displacement from Center (um)');
+fitlabel = append(num2str((fit3(1))),' um/MHz ->', num2str((xfitoffcham)), ' um/MHz inside');
+legend('Spot center', fitlabel, 'Location', 'southeast');
+
+
+f4 = figure;
+plot(freq0([1,11,21,31,41,51]), ydisp([1,11,21,31,41,51]),'*');
+hold on;
+plot(freq0([1,11,21,31,41,51]), yfitoff, '--');
 title('CH0 Displacement vs frequency Off Axis');
 xlabel('CH0 Output Frequency (MHz)');
 ylabel('Displacement from Center (um)');
-fitlabel = append(num2str((fit1(1))),' um/MHz ->', num2str((yfitin)), ' um/MHz inside');
-legend('Spot center', fitlabel, 'Location', 'southwest');
-
+fitlabel = append(num2str((fit4(1))),' um/MHz ->', num2str((yfitoffcham)), ' um/MHz inside');
+legend('Spot center', fitlabel, 'Location', 'southeast');
