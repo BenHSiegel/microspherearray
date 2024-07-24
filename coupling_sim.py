a219c74195a0d07914cbdf13d0be01ae7acbafee
Ben Siegel <b.siegel@yale.edu>
2024-07-24 17:38:57 -0400

    Update coupling_sim.py

--- coupling_sim.py
+++ coupling_sim.py
@@ -13,9 +13,15 @@ def freq_to_k(f):
     return k
 
 def motion_eq(x, x_other, k, CC):
+    #Harmonic oscillator approximation ignoring the constant repulsive force
     acc = -k*x - CC*(x-x_other)
     return acc
 
+def full_motion_eq(x, x_other, k, CC, d):
+    #Non approximated force equation for two negatively charged spheres
+    #direction is included in the CC term
+    acc = -k*x + CC/(d+x_other-x)**2
+    return acc
 
 def position_update(x,v,dt):
     x_new = x + v*dt/2.0
@@ -32,7 +38,7 @@ def random_velocity_update(v,gamma,kBT,dt):
     v_new = c1*v + R*c2
     return v_new
 
-def baoab(motion_eq, timespan, dt, fs, gamma, kBT, pos_init1, pos_init2, vel_init1, vel_init2, k1, k2, CC):
+def baoab(motion_eq, timespan, dt, fs, gamma, kBT, pos_init1, pos_init2, vel_init1, vel_init2, k1, k2, CC, sep):
     
     save_frequency = 1/(dt*fs)
     
@@ -51,8 +57,8 @@ def baoab(motion_eq, timespan, dt, fs, gamma, kBT, pos_init1, pos_init2, vel_ini
     while(t<timespan):
         
         # B
-        force1 = motion_eq(x1,x2,k1,CC)
-        force2 = motion_eq(x2,x1,k2,CC)
+        force1 = full_motion_eq(x1,x2,k1,-CC, sep)
+        force2 = full_motion_eq(x2,x1,k2,CC, sep)
         v1 = velocity_update(v1,force1,dt)
         v2 = velocity_update(v2,force2,dt)
         #A
@@ -65,8 +71,8 @@ def baoab(motion_eq, timespan, dt, fs, gamma, kBT, pos_init1, pos_init2, vel_ini
         x1 = position_update(x1,v1,dt)
         x2 = position_update(x2,v2,dt)
         # B
-        force1 = motion_eq(x1,x2,k1,CC)
-        force2 = motion_eq(x2,x1,k2,CC)
+        force1 = full_motion_eq(x1,x2,k1,-CC, sep)
+        force2 = full_motion_eq(x2,x1,k2,CC, sep)
         v1 = velocity_update(v1,force1,dt)
         v2 = velocity_update(v2,force2,dt)
         
@@ -87,7 +93,7 @@ timespan = 100
 dt = 0.0001
 fs = 1000
 
-pos_ints = [3e-7,-2e-7]
+pos_ints = [3e-7,-2e-7]     #starting position in m
 vel_ints = [0,0]
 
 pressure = 0.4      # in mbar
@@ -100,24 +106,34 @@ f2 = 220            # in Hz
 k1 = freq_to_k(f1)  # in N m^-1 kg^-1
 k2 = freq_to_k(f2)  # in N m^-1 kg^-1
 
-sep = 40            # separation in um
-charge = 2000       # number of electrons
-charge_coupling = 230.708 * charge**2 / (sep)**3 # in N m^-1 kg^-1
+sep = [210, 180, 140, 120, 100, 70, 60, 50]          # separation in um
+figs = {}
+axs = {}
+i = 0
+for d in sep:
+    charge = 6000       # number of electrons
+    charge_const = 2.30708e-16 * charge**2      #Q^2 / (4 pi epsilon_0 * 1ng) in N m^2 / kg
+    
+    #approx_charge_coupling = 230.708 * charge**2 / (d)**3 # in N m^-1 kg^-1
 
-times, positions1, velocities1, positions2, velocities2  = baoab(motion_eq, timespan, dt, fs, gamma, kBT,\
-                                                                    pos_init1=pos_ints[0], pos_init2=pos_ints[1],\
-                                                                    vel_init1=vel_ints[0], vel_init2=vel_ints[1],\
-                                                                    k1=k1, k2=k2, CC=charge_coupling)
+    times, positions1, velocities1, positions2, velocities2  = baoab(motion_eq, timespan, dt, fs, gamma, kBT,\
+                                                                        pos_init1=pos_ints[0], pos_init2=pos_ints[1],\
+                                                                        vel_init1=vel_ints[0], vel_init2=vel_ints[1],\
+                                                                        k1=k1, k2=k2, CC=charge_const, sep=(d*10**-6))
 
 
-segmentsize = round(fs/4)
+    segmentsize = round(fs/5)
 
-freq, PSD1 = welch(positions1[20000:], fs, 'hann')
-freq, PSD2 = welch(positions2[20000:], fs, 'hann')
+    freq, PSD1 = welch(positions1[20000:], fs, 'hann', nperseg=segmentsize)
+    freq, PSD2 = welch(positions2[20000:], fs, 'hann', nperseg=segmentsize)
+    figs[i], axs[i] = plt.subplots()
 
-plt.semilogy(freq,PSD1)
-plt.semilogy(freq,PSD2)
-plt.title("Simulated PSD of Coupled Charged Spheres with Gas Interaction")
-plt.xlabel('Frequency (Hz)')
-plt.ylabel(r'PSD ($m^2/Hz$)')
+    axs[i].semilogy(freq,PSD1)
+    axs[i].semilogy(freq,PSD2)
+    axs[i].set_title("Simulated PSD of Coupled Charged Spheres with Gas Interaction \n at %d um Separation and %d e Charge" % (d, charge))
+    axs[i].set_xlabel('Frequency (Hz)')
+    axs[i].set_ylabel(r'PSD ($m^2/Hz$)')
+
+    i += 1
+
 plt.show()
