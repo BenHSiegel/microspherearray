4a9b3b45c581577d30c349acd8e7c124fe2fa2d0
BenHSiegel <b.siegel@yale.edu>
2024-11-18 18:54:26 -0500

    Update tracker comparison plot.py

--- tracker comparison plot.py
+++ tracker comparison plot.py
@@ -28,4 +28,40 @@ from basichdf5 import hdf5_sphere_data_scraper as scraper
 
 path = r'C:\Users\Ben\Documents\Research\Tracker comparison'
 os.chdir(path)
-x_trackpy, y_trackpy, _, _, _, _ = scraper('ulp1.h5')
+_, _, ASDx_trackpy, ASDy_trackpy, f_trackpy, framerate = scraper('ulp1.h5')
+
+cnn = np.load('cnn_translations_ulp1.npy')
+eigenframe = np.load('eigenframe_translations_ulp1.npy')
+
+fftbinning = 2048
+segmentsize = round(framerate/4)
+fx_eigen, PSDx_eigen = welch(eigenframe[:,0], framerate, 'hann', segmentsize, segmentsize/2, fftbinning, 'constant', True, 'density', 0,'mean')
+fy_eigen, PSDy_eigen = welch(eigenframe[:,1], framerate, 'hann', segmentsize, segmentsize/2, fftbinning, 'constant', True, 'density', 0,'mean')
+fx_cnn, PSDx_cnn = welch(cnn[:,0], framerate, 'hann', segmentsize, segmentsize/2, fftbinning, 'constant', True, 'density', 0,'mean')
+fy_cnn, PSDy_cnn = welch(cnn[:,1], framerate, 'hann', segmentsize, segmentsize/2, fftbinning, 'constant', True, 'density', 0,'mean')
+ASDx_eigen = np.sqrt(PSDx_eigen)
+ASDy_eigen = np.sqrt(PSDy_eigen)
+ASDx_cnn = np.sqrt(PSDx_cnn)
+ASDy_cnn = np.sqrt(PSDy_cnn)
+
+
+fig, ax = plt.subplots()
+ax.semilogy(f_trackpy[:-1], ASDx_trackpy[:-1],label='TrackPy')
+ax.semilogy(fx_eigen[:-1], ASDx_eigen[:-1],label='Eigenframe')
+ax.semilogy(fx_cnn[:-1], ASDx_cnn[:-1],label='CNN')
+
+ax.legend()
+ax.set_xlabel('Frequency (Hz)')
+ax.set_ylabel(r'Amplitude Spectral Density ($m/ \sqrt{Hz}$)')
+
+fig1, ax1 = plt.subplots()
+
+ax1.loglog(f_trackpy[:-1], ASDx_trackpy[:-1],label='TrackPy')
+ax1.loglog(fx_eigen[:-1], ASDx_eigen[:-1],label='Eigenframe')
+ax1.loglog(fx_cnn[:-1], ASDx_cnn[:-1],label='CNN')
+ax1.set_xlim([10,500])
+ax1.legend(fontsize=16,loc='lower left')
+ax1.set_xlabel('Frequency (Hz)',fontsize=18)
+ax1.set_ylabel(r'Amplitude Spectral Density ($m/ \sqrt{Hz}$)',fontsize=18)
+ax1.tick_params(labelsize=14)
+plt.show()
